79f63dcfc8fe31180529a748d7958801c519e5ab
giovannidominguez-ghost <giovanni_dominguez@student.davincischools.org>
2025-12-15 22:44:49 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,52 @@
+// print a message in the Console to understand what is/is not running
+console.log("JavaScript is linked and ready to go!");
+
+function It() {
+  document.getElementById("myVideo").classList.remove("hide");
+  document.getElementById("darkness_image").classList.add("hide");
+  document.getElementById("myVideo").src =
+    "https://www.youtube.com/embed/FnCdOQsX5kc?si=QafcTBzoFuhkpSdc";
+  document.getElementById("changingbio").innerHTML =
+    "𝙰 𝚐𝚛𝚘𝚞𝚙 𝚘𝚏 𝚔𝚒𝚍𝚜 𝚒𝚗 𝙳𝚎𝚛𝚛𝚢, 𝙼𝚊𝚒𝚗𝚎, 𝚏𝚊𝚌𝚎 𝚊 𝚜𝚑𝚊𝚙𝚎-𝚜𝚑𝚒𝚏𝚝𝚒𝚗𝚐 𝚎𝚟𝚒𝚕 𝚝𝚑𝚊𝚝 𝚊𝚙𝚙𝚎𝚊𝚛𝚜 𝚊𝚜 𝙿𝚎𝚗𝚗𝚢𝚠𝚒𝚜𝚎 𝚝𝚑𝚎 𝙲𝚕𝚘𝚠𝚗. 𝙱𝚢 𝚌𝚘𝚗𝚏𝚛𝚘𝚗𝚝𝚒𝚗𝚐 𝚝𝚑𝚎𝚒𝚛 𝚏𝚎𝚊𝚛𝚜 𝚝𝚘𝚐𝚎𝚝𝚑𝚎𝚛, 𝚝𝚑𝚎𝚢 𝚍𝚛𝚒𝚟𝚎 𝚝𝚑𝚎 𝚌𝚛𝚎𝚊𝚝𝚞𝚛𝚎 𝚊𝚠𝚊𝚢 𝚊𝚗𝚍 𝚜𝚞𝚛𝚟𝚒𝚟𝚎 𝚒𝚝𝚜 𝚊𝚝𝚝𝚊𝚌𝚔𝚜.";
+}
+
+function TrickRTreat() {
+  document.getElementById("myVideo").classList.remove("hide");
+  document.getElementById("darkness_image").classList.add("hide");
+  document.getElementById("myVideo").src =
+    "https://www.youtube.com/embed/NAkJuk9Esk4?si=J9e3nml7RTelvj82";
+  document.getElementById("changingbio").innerHTML =
+    "𝙰 𝙷𝚊𝚕𝚕𝚘𝚠𝚎𝚎𝚗 𝚗𝚒𝚐𝚑𝚝 𝚒𝚗 𝚊 𝚜𝚖𝚊𝚕𝚕 𝚝𝚘𝚠𝚗 𝚞𝚗𝚏𝚘𝚕𝚍𝚜 𝚝𝚑𝚛𝚘𝚞𝚐𝚑 𝚏𝚘𝚞𝚛 𝚒𝚗𝚝𝚎𝚛𝚌𝚘𝚗𝚗𝚎𝚌𝚝𝚎𝚍 𝚑𝚘𝚛𝚛𝚘𝚛 𝚜𝚝𝚘𝚛𝚒𝚎𝚜, 𝚊𝚕𝚕 𝚝𝚒𝚎𝚍 𝚝𝚘𝚐𝚎𝚝𝚑𝚎𝚛 𝚋𝚢 𝚊 𝚖𝚢𝚜𝚝𝚎𝚛𝚒𝚘𝚞𝚜 𝚙𝚞𝚖𝚙𝚔𝚒𝚗-𝚑𝚎𝚊𝚍𝚎𝚍 𝚏𝚒𝚐𝚞𝚛𝚎 𝚗𝚊𝚖𝚎𝚍 𝚂𝚊𝚖 𝚠𝚑𝚘 𝚎𝚗𝚏𝚘𝚛𝚌𝚎𝚜 𝚝𝚑𝚎 𝚛𝚞𝚕𝚎𝚜 𝚘𝚏 𝙷𝚊𝚕𝚕𝚘𝚠𝚎𝚎𝚗—𝚊𝚗𝚍 𝚙𝚞𝚗𝚒𝚜𝚑𝚎𝚜 𝚊𝚗𝚢𝚘𝚗𝚎 𝚠𝚑𝚘 𝚋𝚛𝚎𝚊𝚔𝚜 𝚝𝚑𝚎𝚖.";
+}
+function ParanormalActivity() {
+  document.getElementById("myVideo").classList.remove("hide");
+  document.getElementById("darkness_image").classList.add("hide");
+  document.getElementById("myVideo").src =
+    "https://www.youtube.com/embed/gBdbUMTXKIA?si=S6UX8uOHVWxFLIAC";
+  document.getElementById("changingbio").innerHTML =
+    "𝙿𝚊𝚛𝚊𝚗𝚘𝚛𝚖𝚊𝚕 𝙰𝚌𝚝𝚒𝚟𝚒𝚝𝚢: 𝚃𝚑𝚎 𝙶𝚑𝚘𝚜𝚝 𝙳𝚒𝚖𝚎𝚗𝚜𝚒𝚘𝚗 𝚏𝚘𝚕𝚕𝚘𝚠𝚜 𝚝𝚑𝚎 𝙵𝚕𝚎𝚎𝚐𝚎𝚜 𝚏𝚊𝚖𝚒𝚕𝚢, 𝚍𝚒𝚜𝚌𝚘𝚟𝚎𝚛𝚒𝚗𝚐 𝚊 𝚜𝚙𝚎𝚌𝚒𝚊𝚕 𝚌𝚊𝚖𝚎𝚛𝚊 𝚝𝚑𝚊𝚝 𝚛𝚎𝚟𝚎𝚊𝚕𝚜 𝚝𝚑𝚎 𝚍𝚎𝚖𝚘𝚗 𝚃𝚘𝚋𝚢, 𝚠𝚑𝚘 𝚋𝚎𝚐𝚒𝚗𝚜 𝚝𝚊𝚛𝚐𝚎𝚝𝚒𝚗𝚐 𝚝𝚑𝚎𝚒𝚛 𝚍𝚊𝚞𝚐𝚑𝚝𝚎𝚛 𝙻𝚎𝚒𝚕𝚊. 𝙰𝚜 𝚝𝚑𝚎 𝚑𝚊𝚞𝚗𝚝𝚒𝚗𝚐 𝚠𝚘𝚛𝚜𝚎𝚗𝚜, 𝙻𝚎𝚒𝚕𝚊 𝚒𝚜 𝚝𝚊𝚔𝚎𝚗 𝚒𝚗𝚝𝚘 𝚊𝚗𝚘𝚝𝚑𝚎𝚛 𝚍𝚒𝚖𝚎𝚗𝚜𝚒𝚘𝚗, 𝚊𝚗𝚍 𝚃𝚘𝚋𝚢 𝚐𝚊𝚒𝚗𝚜 𝚊 𝚙𝚑𝚢𝚜𝚒𝚌𝚊𝚕 𝚏𝚘𝚛𝚖.";
+}
+function TheThing() {
+  document.getElementById("myVideo").classList.remove("hide");
+  document.getElementById("darkness_image").classList.add("hide");
+  document.getElementById("myVideo").src =
+    "https://www.youtube.com/embed/Zad25YEW91Q?si=jPwjMduOLbG5k6X1";
+  document.getElementById("changingbio").innerHTML =
+    "𝙰 𝚜𝚑𝚊𝚙𝚎-𝚜𝚑𝚒𝚏𝚝𝚒𝚗𝚐 𝚊𝚕𝚒𝚎𝚗 𝚒𝚜 𝚞𝚗𝚌𝚘𝚟𝚎𝚛𝚎𝚍 𝚒𝚗 𝙰𝚗𝚝𝚊𝚛𝚌𝚝𝚒𝚌𝚊 𝚊𝚗𝚍 𝚋𝚎𝚐𝚒𝚗𝚜 𝚒𝚖𝚒𝚝𝚊𝚝𝚒𝚗𝚐 𝚝𝚑𝚎 𝚛𝚎𝚜𝚎𝚊𝚛𝚌𝚑𝚎𝚛𝚜 𝚊𝚝 𝚊 𝚛𝚎𝚖𝚘𝚝𝚎 𝚘𝚞𝚝𝚙𝚘𝚜𝚝. 𝙰𝚜 𝚙𝚊𝚛𝚊𝚗𝚘𝚒𝚊 𝚜𝚙𝚛𝚎𝚊𝚍𝚜 𝚊𝚋𝚘𝚞𝚝 𝚠𝚑𝚘 𝚒𝚜 𝚑𝚞𝚖𝚊𝚗 𝚊𝚗𝚍 𝚠𝚑𝚘 𝚒𝚜𝚗’𝚝, 𝚝𝚑𝚎 𝚜𝚞𝚛𝚟𝚒𝚟𝚘𝚛𝚜 𝚠𝚘𝚛𝚔 𝚝𝚘𝚐𝚎𝚝𝚑𝚎𝚛 𝚝𝚘 𝚍𝚎𝚜𝚝𝚛𝚘𝚢 𝚝𝚑𝚎 𝚌𝚛𝚎𝚊𝚝𝚞𝚛𝚎 𝚋𝚎𝚏𝚘𝚛𝚎 𝚒𝚝 𝚎𝚜𝚌𝚊𝚙𝚎𝚜.";
+}
+function Halloween2018() {
+  document.getElementById("myVideo").classList.remove("hide");
+  document.getElementById("darkness_image").classList.add("hide");
+  document.getElementById("myVideo").src =
+    "https://www.youtube.com/embed/R-HV-gTRqag?si=VsXRvKEIVAIZHAJ4";
+  document.getElementById("changingbio").innerHTML =
+    "𝙵𝚘𝚛𝚝𝚢 𝚢𝚎𝚊𝚛𝚜 𝚊𝚏𝚝𝚎𝚛 𝚜𝚞𝚛𝚟𝚒𝚟𝚒𝚗𝚐 𝙼𝚒𝚌𝚑𝚊𝚎𝚕 𝙼𝚢𝚎𝚛𝚜’ 𝚊𝚝𝚝𝚊𝚌𝚔, 𝙻𝚊𝚞𝚛𝚒𝚎 𝚂𝚝𝚛𝚘𝚍𝚎 𝚒𝚜 𝚜𝚝𝚒𝚕𝚕 𝚙𝚛𝚎𝚙𝚊𝚛𝚒𝚗𝚐 𝚏𝚘𝚛 𝚑𝚒𝚜 𝚛𝚎𝚝𝚞𝚛𝚗. 𝚆𝚑𝚎𝚗 𝙼𝚒𝚌𝚑𝚊𝚎𝚕 𝚎𝚜𝚌𝚊𝚙𝚎𝚜 𝚍𝚞𝚛𝚒𝚗𝚐 𝚊 𝚙𝚛𝚒𝚜𝚘𝚗 𝚝𝚛𝚊𝚗𝚜𝚏𝚎𝚛, 𝚑𝚎 𝚐𝚘𝚎𝚜 𝚘𝚗 𝚊𝚗𝚘𝚝𝚑𝚎𝚛 𝚔𝚒𝚕𝚕𝚒𝚗𝚐 𝚜𝚙𝚛𝚎𝚎. 𝙻𝚊𝚞𝚛𝚒𝚎, 𝚑𝚎𝚛 𝚍𝚊𝚞𝚐𝚑𝚝𝚎𝚛, 𝚊𝚗𝚍 𝚐𝚛𝚊𝚗𝚍𝚍𝚊𝚞𝚐𝚑𝚝𝚎𝚛 𝚛𝚎𝚞𝚗𝚒𝚝𝚎 𝚝𝚘 𝚎𝚗𝚍 𝚑𝚒𝚖. ";
+}
+function Poltergeist() {
+  document.getElementById("myVideo").classList.remove("hide");
+  document.getElementById("darkness_image").classList.add("hide");
+  document.getElementById("myVideo").src =
+    "https://www.youtube.com/embed/RtKn4rHAMAg?si=uVxHz1N9rX4GwjOl";
+  document.getElementById("changingbio").innerHTML =
+    "𝙰 𝚜𝚞𝚋𝚞𝚛𝚋𝚊𝚗 𝚏𝚊𝚖𝚒𝚕𝚢’𝚜 𝚑𝚘𝚖𝚎 𝚋𝚎𝚌𝚘𝚖𝚎𝚜 𝚑𝚊𝚞𝚗𝚝𝚎𝚍 𝚠𝚑𝚎𝚗 𝚜𝚙𝚒𝚛𝚒𝚝𝚜 𝚌𝚘𝚖𝚖𝚞𝚗𝚒𝚌𝚊𝚝𝚎 𝚝𝚑𝚛𝚘𝚞𝚐𝚑 𝚝𝚑𝚎 𝚃𝚅 𝚊𝚗𝚍 𝚊𝚋𝚍𝚞𝚌𝚝 𝚝𝚑𝚎𝚒𝚛 𝚢𝚘𝚞𝚗𝚐 𝚍𝚊𝚞𝚐𝚑𝚝𝚎𝚛, 𝙲𝚊𝚛𝚘𝚕 𝙰𝚗𝚗𝚎. 𝙰𝚜 𝚝𝚑𝚎 𝚑𝚊𝚞𝚗𝚝𝚒𝚗𝚐 𝚒𝚗𝚝𝚎𝚗𝚜𝚒𝚏𝚒𝚎𝚜, 𝚝𝚑𝚎 𝚏𝚊𝚖𝚒𝚕𝚢 𝚜𝚎𝚎𝚔𝚜 𝚑𝚎𝚕𝚙 𝚏𝚛𝚘𝚖 𝚙𝚊𝚛𝚊𝚗𝚘𝚛𝚖𝚊𝚕 𝚎𝚡𝚙𝚎𝚛𝚝𝚜 𝚝𝚘 𝚛𝚎𝚜𝚌𝚞𝚎 𝚑𝚎𝚛 𝚏𝚛𝚘𝚖 𝚝𝚑𝚎 𝚜𝚞𝚙𝚎𝚛𝚗𝚊𝚝𝚞𝚛𝚊𝚕 𝚛𝚎𝚊𝚕𝚖.";
+}
